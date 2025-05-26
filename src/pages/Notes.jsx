a95a1b66f97ea95d25b7d2436965e3a00eb842f4
Juan Agudelo <verojuan9007@gmail.com>
2025-05-26 15:24:28 -0500

    page notes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 
 function Notes() {
+  const [cards, sertCards] = useState([{ id: 1, text: "" }]);
+
   return (
-    <div>Notes</div>
-  )
+    <div>
+      <div
+        id="container"
+        className="w-full min-h-screen grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 place-items-start border-black bg-black"
+      >
+        <div
+          id="cards"
+          className="w-60 h-60 border-black bg-yellow-200 mt-8 text-center"
+        >
+          hola me llamo chichi
+        </div>
+        <div
+          id="cards"
+          className="w-60 h-60 border-black bg-yellow-200 mt-8 text-center"
+        >
+          hola me llamo chichi
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Notes
+export default Notes;
