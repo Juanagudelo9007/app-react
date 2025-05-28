b24b4a13a3e91949bcba8092d44523989a8a51bc
Juan Agudelo <verojuan9007@gmail.com>
2025-05-28 14:13:34 -0500

    working on contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useSafeLayoutEffect } from "@chakra-ui/react";
+import { set } from "firebase/database";
+import React, { useState } from "react";
 
 function Contact() {
+  const [contactOpen, setContactOpen] = useState(false);
+
+  const formOpen = () => setContactOpen(true);
+  const formClose = () => setContactOpen(false);
+
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div>
+        <div>{setcon}</div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
