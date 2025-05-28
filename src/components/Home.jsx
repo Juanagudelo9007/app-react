71f7867e7e74a26c910a9a34e96c216439834772
Juan Agudelo <verojuan9007@gmail.com>
2025-05-27 22:22:12 -0500

    the welcome text from the home page has been animated with framer motion, im going to use framer motion for now and  for a next level animation i will use gsap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <div className="min-h-screen bg-cover bg-center bg-[url('https://plus.unsplash.com/premium_photo-1670179694048-5e4a1dcad9b6?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] font-sans">
       <div className="p-6 max-w-7xl mx-auto text-center">
-        <p className="text-4xl sm:text-6xl md:text-6xl font-extrabold text-[#e8e8e8]">
+        <motion.p
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1.1 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="text-4xl sm:text-6xl md:text-6xl font-extrabold text-[#e8e8e8]"
+        >
           Welcome Back!
-        </p>
+        </motion.p>
         <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8">
           <div className="bg-white/0 backdrop-blur-sm border border-white/30 p-6 rounded-[10px] shadow-[3px_2px_6px_2px_rgba(0,0,0,0.3)] cursor-pointer mt-4 hover:scale-110 transition-all duration-500 text-white font-bold hover:underline">
             <h2>Notes</h2>
